9d4fcd73d7cf4f4980c723dc71345bf443deaf31
Sandip <ssarwate@gmail.com>
2015-05-13 18:53:13 -0700

    Featured series
    Show the Summer series as the featured series. Final design changes
    still to be done.

---
+++ custom/series/summer/summer.php
@@ -0,0 +1,7 @@
+<div class="panel clearfix">
+  <figure>
+    <img class="circular" alt="Summer Series" src="<?php echo $path; ?>/starry_nights.jpg" width="362" height="362" /></a>
+  </figure>
+  <p>We&#8217;re here to help you enjoy this beloved season in all its glory&mdash;open the windows, let the sunshine in, change things around and embrace color. Use Saffron Marigold linens to dazzle and cheer up your home</p>
+  <p class="read-more"><a href="/blog/saffron-marigolds-summer-series-ideas-to-redecorate-for-summer/"><span class="pre">Read our</span><span class="title">Summer Series</span></a></p>
+</div>
