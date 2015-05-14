7e887cb99eb1b3fb0bdef9ac753340f494cac199
Vladimir Stamatovic <stamat@mrgud>
2015-05-14 17:26:10 +0200

    Add summer box. Corrected git structure

--- custom/series/summer/summer.php
+++ custom/series/summer/summer.php
@@ -1,12 +1,31 @@
+<?php /*
 <div class="shopping-guide post-listing summer-series">
     <p>What better day to launch our Summer Series than today&mdash;the start of summer! Summer conjures up visions of beautiful, balmy, lazy, sunny days&hellip; laying on a hammock, backyard barbecues, vacations across the country, trips to the beach, lemonade and chilled beer.</p>
 
-    <div class="panel clearfix">
+    <div class="panel clearfix panel-summer">
         <figure>
             <a href="http://www.saffronmarigold.com/blog/saffron-marigolds-summer-series-ideas-to-redecorate-for-summer/"><img class="circular" alt="Paisley Fraiche Duvet Cover" src="<?php echo $path; ?>/paisley-fraiche.jpg" width="362" height="362" /></a>
             <figcaption>Paisley Fraiche Duvet Cover and Shams</figcaption>
         </figure>
        <p>We&#8217;re here to help you enjoy this beloved season in all its glory&mdash;open the windows, let the sunshine in, change things around and embrace color. Use Saffron Marigold linens to dazzle and cheer up your <a href="http://www.saffronmarigold.com/blog/saffron-marigolds-summer-series-ideas-to-redecorate-for-summer/#bedrooms" >bedrooms</a>, <a href="http://www.saffronmarigold.com/blog/saffron-marigolds-summer-series-ideas-to-redecorate-for-summer/#dining-rooms">dining rooms</a>, <a href="http://www.saffronmarigold.com/blog/saffron-marigolds-summer-series-ideas-to-redecorate-for-summer/#living">living spaces</a> and <a href="http://www.saffronmarigold.com/blog/saffron-marigolds-summer-series-ideas-to-redecorate-for-summer/#bath">baths</a>!</p>
        <p class="read-more"><a href="http://www.saffronmarigold.com/blog/saffron-marigolds-summer-series-ideas-to-redecorate-for-summer/">Read Summer Series</a></p>
+    </div>
+</div> */
+?>
+<div class="shopping-guide post-listing summer-series">
+    <p>What better day to launch our Summer Series than today&mdash;the start of summer! Summer conjures up visions of beautiful, balmy, lazy, sunny days&hellip; laying on a hammock, backyard barbecues, vacations across the country, trips to the beach, lemonade and chilled beer.</p>
+
+    <div class="panel clearfix panel-summer">
+        <figure>
+            <a href="http://www.saffronmarigold.com/blog/saffron-marigolds-summer-series-ideas-to-redecorate-for-summer/"><img class="circular" alt="Paisley Fraiche Duvet Cover" src="<?php echo $path; ?>/tg_dr_main.jpg" width="362" height="362" /></a>
+            <figcaption>Paisley Fraiche Duvet Cover and Shams</figcaption>
+        </figure>
+        <p>
+       <b class="top">W</b>hat better day to launch our Summer Series than today – the start of summer!</p>
+		<p class="inner">Summer conjures up visions of beautiful, <b>balmy</b>, <b>lazy</b>, <b>sunny days</b>…..laying on a hammock, backyard barbecues, vacations across the country, trips to the beach, lemonade and chilled beer.</p>
+		<p>
+		<b class="more"><a href="http://www.saffronmarigold.com/blog/saffron-marigolds-summer-series-ideas-to-redecorate-for-summer/">See more…</a></b>
+		</p>
+      <!-- <p class="read-more"><a href="http://www.saffronmarigold.com/blog/saffron-marigolds-summer-series-ideas-to-redecorate-for-summer/">Read Summer Series</a></p>-->
     </div>
 </div>
