bf806bbf7a766c75b03b3f47908095fc0edf5dd0
Sarah (test server) <sarah@triggersandsparks.com>
2015-05-29 15:14:51 -0400

    Summer series edits made by Elena

--- custom/series/summer/summer.php
+++ custom/series/summer/summer.php
@@ -3,7 +3,7 @@
         <figure>
             <a href="http://www.saffronmarigold.com/blog/saffron-marigolds-summer-series-ideas-to-redecorate-for-summer/"><img class="circular" alt="Summer Series" src="<?php echo $path; ?>/starry_nights.jpg" width="362" height="362" /></a>
         </figure>
-  <p>We&#8217;re here to help you enjoy this beloved season in all its glory&mdash;open the windows, let the sunshine in, change things around and embrace color. Use Saffron Marigold linens to dazzle and cheer up your home</p>
-  <p class="read-more"><a href="/blog/saffron-marigolds-summer-series-ideas-to-redecorate-for-summer/"><span class="pre">Read our</span><span class="title">Summer Series</span></a></p>
+  <p>We&#8217;re here to <span>help</span> you <span>enjoy</span> this beloved<br /> season in all its <span>glory</span>&mdash;open the windows, let the <span>sunshine</span> in, change things around and embrace <span>color</span>. Use <span>Saffron Marigold</span> linens to <span>dazzle</span> and <span>cheer up</span> your home</p>
+  <p align="right"><a href="/blog/saffron-marigolds-summer-series-ideas-to-redecorate-for-summer/">See More <img src="<?php echo $path; ?>/arrow.png" align="right" class="arrow" /></a></p>
     </div>
 </div>
